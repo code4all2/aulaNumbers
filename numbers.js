6473a0b082dc3e2f3d09574f3337551743a2c4e7
code4all2 <code4allaulaonline@gmail.com>
2023-04-13 10:06:48 -0300

    proj de aula  numbers

---
+++ numbers.js
@@ -0,0 +1,10 @@
+// + - * / %
+// && ;  ||; < ;  >; <= ; >= ;  ==; ===; !=; !==
+let num = 9.4215781452;
+
+
+//MATH
+let n2 = Math.floor(num);
+let n3 = Math.ceil(num);
+let n4 = Math.round(num);
+console.log(n4);
